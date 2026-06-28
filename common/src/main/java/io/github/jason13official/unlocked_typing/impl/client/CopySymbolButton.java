85b0210b639f074b3458cc784cb59880a20e5209
jason13official <jason13official@gmail.com>
2026-06-28 13:19:03 -0400

    do some porting

---
+++ common/src/main/java/io/github/jason13official/unlocked_typing/impl/client/CopySymbolButton.java
@@ -0,0 +1,38 @@
+package io.github.jason13official.unlocked_typing.impl.client;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.PlainTextButton;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
+import net.minecraft.util.Mth;
+
+public class CopySymbolButton extends PlainTextButton {
+
+  public CopySymbolButton() {
+    super(80, 0, 80, 16, Component.literal("COPY SYMBOL").withStyle(Style.EMPTY), button -> ClipboardHelper.copyToClipboard("§"), Minecraft.getInstance().font);
+  }
+
+  @Override
+  public void renderWidget(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
+    Minecraft minecraft = Minecraft.getInstance();
+    guiGraphics.setColor(1.0F, 1.0F, 1.0F, this.alpha);
+    RenderSystem.enableBlend();
+    RenderSystem.enableDepthTest();
+    guiGraphics.blitNineSliced(WIDGETS_LOCATION, this.getX(), this.getY(), this.getWidth(), this.getHeight(), 20, 4, 200, 20, 0, this.getTextureY());
+    guiGraphics.setColor(1.0F, 1.0F, 1.0F, 1.0F);
+    int i = this.active ? 16777215 : 10526880;
+    this.renderString(guiGraphics, minecraft.font, i | Mth.ceil(this.alpha * 255.0F) << 24);
+  }
+
+  private int getTextureY() {
+    int i = 1;
+    if (!this.active) {
+      i = 0;
+    } else if (this.isHoveredOrFocused()) {
+      i = 2;
+    }
+    return 46 + i * 20;
+  }
+}
---
+++ common/src/main/java/io/github/jason13official/unlocked_typing/mixin/client/BookEditScreenMixin.java
@@ -0,0 +1,48 @@
+package io.github.jason13official.unlocked_typing.mixin.client;
+
+import io.github.jason13official.unlocked_typing.api.client.accessor.ScreenAccessor;
+import io.github.jason13official.unlocked_typing.impl.client.CopySymbolButton;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.font.TextFieldHelper;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.BookEditScreen;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BookEditScreen.class)
+public abstract class BookEditScreenMixin {
+
+  @Shadow
+  private boolean isSigning;
+
+  @Shadow
+  private String title;
+
+  @Shadow
+  @Final
+  private TextFieldHelper titleEdit;
+
+  /// Allows pasting into the book title field during signing
+  @Inject(at = @At("HEAD"), method = "titleKeyPressed", cancellable = true)
+  private void unlocked_typing$titleKeyPressed(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
+
+    if (this.isSigning && Screen.isPaste(keyCode) && this.title.length() < 30) {
+      this.titleEdit.insertText(TextFieldHelper.getClipboardContents(Minecraft.getInstance()));
+      cir.setReturnValue(true);
+    }
+  }
+
+  @Inject(at = @At("TAIL"), method = "init()V")
+  private void unlocked_typing$init(CallbackInfo ci) {
+
+    Screen self = (Screen) (Object) this;
+    ScreenAccessor accessor = (ScreenAccessor) self;
+
+    accessor.unlocked_typing$addRenderableWidget(new CopySymbolButton());
+  }
+}
